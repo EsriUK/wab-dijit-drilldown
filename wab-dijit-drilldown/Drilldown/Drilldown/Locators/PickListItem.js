d2cdf8fa2fabe2a59dd3d0b238edf849fbb09fbb
Travis CI <sbatterby@esriuk.com>
2015-10-13 08:58:39 +0000

    Update from travis build of arcgis-dijit-drilldown

---
+++ wab-dijit-drilldown/Drilldown/Drilldown/Locators/PickListItem.js
@@ -0,0 +1,1 @@
+define(["dojo/_base/declare"],function(a){return a(null,{Description:"",Addresses:[],Level:0,SortDescription:"",constructor:function(b){this.Description="",this.Addresses=[],this.Level=0,this.SortDescription="",a.safeMixin(this,b)},addCandidate:function(a){this.Addresses.push(a)}})});
